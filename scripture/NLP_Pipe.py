88e84cc2ec75c87189db36938473201b2f356fd6
Matt <beatoven7@gmail.com>
2018-09-05 13:27:41 -0400

    1:30

--- scripture/NLP_Pipe.py
+++ scripture/NLP_Pipe.py
@@ -14,6 +14,40 @@ def nlp_pipe(ads):
     #Create corpus for ads in which conversion rate an ad copy have no nans
     # corp=[ads.loc[x,'ad_copy'] for x in range(len(ads))]
     
+    conv_ads = ads[~ads['ad_copy'].isna()]
+    conv_ads.reset_index(inplace=True)
+    conv_corp=[conv_ads.loc[x,'ad_copy'] for x in range(len(conv_ads))]
+
+    lemmed_conv_corp=[]
+    for word in range(len(conv_corp)):
+        if conv_corp[word]!= None:
+            lemmed_conv_corp.append(wordnet.lemmatize(conv_corp[word]))        
+
+    #TFIDF
+    tf_conv = TfidfVectorizer(stop_words='english')
+    conv_corp_vect=tf_conv.fit_transform(lemmed_conv_corp)
+
+    #Word count vector
+    count = CountVectorizer(stop_words='english')
+    counted_corp = count.fit_transform(lemmed_conv_corp)
+    sum_words = counted_corp.sum(axis=0)
+    words_freq = [(word, sum_words[0,idx]) for word, idx in count.vocabulary_.items()]
+    words_freq =sorted(words_freq, key = lambda x: x[1], reverse=True)
+
+    return tf_conv, count, conv_corp_vect, counted_corp, words_freq
+
+
+def nlp_pipe_conversion(ads):
+    '''
+    Pipeline that returns the tfidf vectorizer, count vectorizer, 
+    tfidf matrix, word count matrix, and sorted word dictionary
+    of the count matrix
+    '''
+    wordnet = WordNetLemmatizer()
+    
+    #Create corpus for ads in which conversion rate an ad copy have no nans
+    # corp=[ads.loc[x,'ad_copy'] for x in range(len(ads))]
+    
     conv_ads = ads[(~ads['conversion_rate'].isna()) & (~ads['ad_copy'].isna())]
     conv_ads.reset_index(inplace=True)
     conv_corp=[conv_ads.loc[x,'ad_copy'] for x in range(len(conv_ads))]
--- web_app/app.py
+++ web_app/app.py
@@ -1,18 +1,62 @@
 from flask import Flask, request, render_template
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+import pandas as pd
+import pickle
+import re
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('landing.html', methods = ['GET'])
+    return render_template('landing.html', methods = ['GET', 'POST'])
 
 
 @app.route('/AFAM/', methods = ['GET'])
 def AFAM():
-    return render_template('profile.html')
+    return render_template('African_American.html')
+
+@app.route('/Progressive/', methods = ['GET'])
+def progressive():
+    return render_template('')
+
+@app.route('/model_form/', methods=['GET', 'POST'])
+def model_form():
+    return render_template('register.html')
+
+@app.route('/predict_page/', methods=['GET','POST'])
+def prediction_page():
+    # load the model
+    with open('data/mk_model.pkl', 'rb') as f:
+        model = pickle.load(f)
 
 
+
+
+
+    
+    #lemmatize
+    wordnet = WordNetLemmatizer()
+    text = request.form['words']
+
+    lemmed_corp=[]
+    for word in range(len(text)):
+        if text[word]!= None:
+            lemmed_corp.append(wordnet.lemmatize(text[word]))
+
+    with open('data/count.pkl', 'rb') as g:
+        count = pickle.load(g)
+    matrix = count.transform(lemmed_corp)
+
+    #Predict categories based on word count
+    preds  = model.predict(matrix)
+
+    
+    return render_template('predict0.html')
+
+    
 
 
 
